36e441667ce33ecbbece5a424b1a2898aa047cd6
Carolina Menezes <carolinamenezes@hotmail.com.br>
2021-02-24 19:32:22 -0300

    Update descriptions

--- src/commands/deps/diff.ts
+++ src/commands/deps/diff.ts
@@ -1,19 +1,34 @@
-import { CustomCommand, SessionManager } from 'vtex'
+import { CustomCommand, SessionManager, ColorifyConstants } from 'vtex'
 import workspaceDepsDiff from '../../modules/deps/diff'
 
 export default class DepsDiff extends CustomCommand {
-  static description =
-    'Diff between workspace dependencies. If only one parameter is passed the current workspace is used in the diff and if no parameter is passed the diff is made between the current workspace and master'
+  static description = `Displays the differences between the dependencies of two distinct ${ColorifyConstants.ID(
+    'workspaces'
+  )}. If a single parameter is passed, the specified ${ColorifyConstants.ID(
+    "workspace's"
+  )} dependencies are compared with the master\'s. If no parameter is passed, the diff is made between the current ${ColorifyConstants.ID(
+    'workspace'
+  )} and master.`
 
-  static examples = ['vtex deps diff workspace1 workspace2']
+  static examples = [`${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex deps diff')} workspace1 workspace2`]
 
   static flags = {
     ...CustomCommand.globalFlags,
   }
 
   static args = [
-    { name: 'workspace1', required: false, default: SessionManager.getSingleton().workspace },
-    { name: 'workspace2', required: false, default: 'master' },
+    {
+      name: 'workspace1',
+      required: false,
+      default: SessionManager.getSingleton().workspace,
+      description: `First ${ColorifyConstants.ID('workspace')} for comparison.`,
+    },
+    {
+      name: 'workspace2',
+      required: false,
+      default: 'master',
+      description: `Second ${ColorifyConstants.ID('workspace')} for comparison.`,
+    },
   ]
 
   async run() {
